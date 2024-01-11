de315d33458a073f3cbc64523199eaed6b6aa2a9
Sukhpreet Singh <peadevp@gmail.com>
2024-01-11 14:51:19 +0530

    added button component

---
+++ src/routes/(public)/ui/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Button from "$lib/ui/Button.svelte"
+
+	const style1 = `
+      bg-layer-9 text-layer-1
+      hover:bg-layer-10
+      focus-visible:bg-layer-10 focus-visible:ring-layer-8 focus-visible:ring-offset-layer-0
+      active:bg-layer-11
+    `
+</script>
+
+<main class="min-h-screen flex items-center justify-center">
+	<Button
+		class="
+      inline-flex items-center justify-center
+      rounded-md px-2.5 py-1 transition-colors ease-in
+      focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-2
+       select-none
+      "
+	>
+		Click Me
+	</Button>
+</main>
