474bf71ba6d5cc12aa9c78be5bff8ef7e0a8baf3
Sukhpreet Singh <sukhpreetben10@gmail.com>
2024-02-01 02:00:33 +0530

    added safe guard for admin user

---
+++ src/routes/admin/+layout.server.ts
@@ -0,0 +1,6 @@
+import { redirect } from "@sveltejs/kit";
+import type { LayoutServerLoad } from "./$types";
+
+export const load: LayoutServerLoad = async (event) => {
+    if(event.locals.user?.role !== 'admin') redirect(308,'/')
+};
