3577af889b42e82c33491bd3f2bfc945cf0c1f64
Sukhpreet Singh <sukhpreetben10@gmail.com>
2024-02-01 10:20:04 +0530

    removed snap points

--- src/lib/components/feedback-dialog.svelte
+++ src/lib/components/feedback-dialog.svelte
@@ -71,7 +71,7 @@
 		</Dialog.Content>
 	</Dialog.Root>
 {:else}
-	<Drawer.Root bind:open snapPoints={[0.4,0.6]} activeSnapPoint={0.6} >
+	<Drawer.Root bind:open>
 		<Drawer.Trigger
 			class={buttonVariants({
 				size: 'responsive',
