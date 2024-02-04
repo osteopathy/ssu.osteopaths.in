f0b642f581fc5213e0fc7bb7c57207ee4925a3c0
Sukhpreet Singh <sukhpreetben10@gmail.com>
2024-02-04 10:20:58 +0530

    added `$api` alias

--- svelte.config.js
+++ svelte.config.js
@@ -12,6 +12,11 @@ const config = {
         // If your environment is not supported or you settled on a specific environment, switch out the adapter.
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter()
+    },
+    alias: {
+        // an alias ending /* will only match
+        // the contents of a directory, not the directory itself
+        '$api/*': 'src/routes/(api)/*'
     }
 };
 
