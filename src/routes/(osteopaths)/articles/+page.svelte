781c86be18e9bce89c4874080ce2ddaafada8572
Sukhpreet Singh <sukhpreetben10@gmail.com>
2024-02-14 10:29:06 +0530

    formated articles +page.svelte

--- src/routes/(osteopaths)/articles/+page.svelte
+++ src/routes/(osteopaths)/articles/+page.svelte
@@ -8,10 +8,10 @@
 		<h2 class="mb-4 text-5xl">Explore Our Courses</h2>
 	</div>
 	<ul class="mt-12 grid gap-12 sm:grid-cols-2">
-		<div class="rounded-xl border bg-card-alt p-6 shadow-inner shadow-muted/75">
+		<div class="bg-card-alt shadow-muted/75 rounded-xl border p-6 shadow-inner">
 			<div class="mb-4 flex flex-col gap-y-1">
 				<h2 class="text-2xl sm:text-3xl">Introduction to Osteopathy</h2>
-				<p class="text-xl text-muted-foreground">
+				<p class="text-muted-foreground text-xl">
 					Osteopathy is based on the idea that all the body's systems are interrelated. Osteopaths
 					focus on treating the whole person, finding the root cause of problem
 				</p>
