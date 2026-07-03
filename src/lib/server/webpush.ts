4b0e44bbbf7ec89590e7201069d00f3b9db087f6
Sukhpreet Singh <sukhpreetben10@gmail.com>
2026-07-03 09:31:38 +0000

    change

--- src/lib/server/webpush.ts
+++ src/lib/server/webpush.ts
@@ -1,8 +1,9 @@
 import webPush from "web-push";
 import { env } from "$env/dynamic/private";
+import { env as publicEnv } from '$env/dynamic/public';
 
-const VAPID_PUBLIC_KEY = env.PUBLIC_VAPID_KEY;
-const VAPID_PRIVATE_KEY = env.PRIVATE_VAPID_KEY;
+const VAPID_PUBLIC_KEY = publicEnv.PUBLIC_VAPID_KEY;
+const VAPID_PRIVATE_KEY = env.VAPID_PRIVATE_KEY;
 const VAPID_SUBJECT = env.VAPID_SUBJECT;
 
 webPush.setVapidDetails(VAPID_SUBJECT, VAPID_PUBLIC_KEY, VAPID_PRIVATE_KEY);
