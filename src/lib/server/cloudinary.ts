d99997a153761c44ee8608c28917742e7d2553d6
Sukhpreet Singh <sukhpreetben10@gmail.com>
2026-07-03 09:22:02 +0000

    12

--- src/lib/server/cloudinary.ts
+++ src/lib/server/cloudinary.ts
@@ -1,5 +1,4 @@
 import { v2 as cloudinary } from "cloudinary";
-import { env as publicEnv } from "$env/dynamic/public";
 import { env } from "$env/dynamic/private";
 import { env as publicEnv } from '$env/dynamic/public';
 
